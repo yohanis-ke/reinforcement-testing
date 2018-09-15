5bf88eaa7345be16f7a5c8a2d61f57cc7b4d146d
yohanis-ke <yohanes.ke@gmail.com>
2018-09-15 11:05:15 -0400

    test_to_hash_key_has_corect_value is done

--- test_team.rb
+++ test_team.rb
@@ -10,4 +10,10 @@ class TestTeam < MiniTest::Test
     expected = Hash
     assert_equal(expected, actual)
   end
+  def test_to_hash_key_has_corect_value
+    setup = Team.new('manchester', '1st', 'Lukaku').to_hash
+    actual = setup[:team_name]
+    expected = 'manchester'
+    assert_equal(actual, expected)
+  end
 end
